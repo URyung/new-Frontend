7a790995467bec214a07891270a8788de3f04f2b
gmlsanfro <gmlsanfro@ajou.ac.kr>
2025-03-18 16:13:09 +0900

    js study ing

---
+++ Frontend/study/js/3_declaring_variable.js
@@ -0,0 +1,33 @@
+/**
+ * Variable 선언하기
+ * 
+ * 1) var - 더이상 쓰지 않는다. 공부하면서는 쓴다
+ * 2) let - 실제로 쓰는 , 프로젝트를 진행할때 
+ * 3) const - //
+ */
+var name = '코드팩토리';
+console.log(name);
+
+var age = 32;
+console.log(age);
+
+let ive = '아이브';
+console.log(ive);
+
+/**
+ * let과 var로 선언하면
+ * 값을 추후 변경할 수 있다.
+ */
+
+ive = '안유진';
+console.log(ive);
+
+
+const newJeans = '뉴진스';
+console.log(newJeans);
+
+// newJeans = '코드팩토리';
+
+/**
+ * 
+ */
